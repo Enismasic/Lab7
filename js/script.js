1a70c82a76030b417932f097b5c27aa65dbba0c8
Enismasic <emasic@hawk.iit.edu>
2022-11-06 17:39:13 -0600

    added js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
+ function nameAlert(){
+	 var input = document.getElementById('entryinput').value;
+	 alert("Enis Masic:" + input);
+	 
+	 var output = document.getElementById('textoutput');
+	 output.innerHTML = userInput;
+ }
  
-
-
-
-
-
-
-
+ var alertButton = document.getElementById('entrybutton');
+ alertButton.addEventListener('click', nameAlert);
+}
 window.addEventListener('load', init);
