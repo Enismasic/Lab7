e8776c7ce548f3888091a418375d21a4c3233ad4
Enismasic <emasic@hawk.iit.edu>
2022-11-06 17:49:36 -0600

    finishing touches

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function init(){
 //add your javascrip between these two lines of code
  function nameAlert(){
 	 var input = document.getElementById('entryinput').value;
-	 alert("Enis Masic:" + input);
+	 alert("Enis Masic:" + input);//obtain the input 
 	 
 	 var output = document.getElementById('textoutput');
-	 output.innerHTML = userInput;
+	 output.innerHTML = input;//obtain the output from input
  }
  
  var alertButton = document.getElementById('entrybutton');
- alertButton.addEventListener('click', nameAlert);
+ alertButton.addEventListener('click', nameAlert); //button function
 }
 window.addEventListener('load', init);
